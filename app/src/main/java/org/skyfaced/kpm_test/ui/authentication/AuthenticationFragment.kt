922da1164b17d34f133f91d462bfc8f992471557
Atabayev Muslim <5106174-SkyfaceD@users.noreply.gitlab.com>
2021-08-08 16:21:20 +0600

    Add authentication view

--- app/src/main/java/org/skyfaced/kpm_test/ui/authentication/AuthenticationFragment.kt
+++ app/src/main/java/org/skyfaced/kpm_test/ui/authentication/AuthenticationFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import org.skyfaced.kpm_test.R
 import org.skyfaced.kpm_test.databinding.FragmentAuthenticationBinding
 import org.skyfaced.kpm_test.ui.BaseFragment
@@ -13,11 +12,6 @@ class AuthenticationFragment :
     BaseFragment<FragmentAuthenticationBinding>(R.layout.fragment_authentication) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.btn.setOnClickListener {
-            val destination =
-                AuthenticationFragmentDirections.actionAuthenticationFragmentToMultipleBackStackGraph()
-            findNavController().navigate(destination)
-        }
     }
 
     override fun setupBinding(
